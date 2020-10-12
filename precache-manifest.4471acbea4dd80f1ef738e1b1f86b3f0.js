f2dc028120acc4ce1830ac3f5887ef5ae9975d3c
Artem <artem@artemsMac.local>
2020-10-12 14:11:33 +0200

    Updates

---
+++ precache-manifest.4471acbea4dd80f1ef738e1b1f86b3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13b45a21325e14f1d5e721f718ac85e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c0e8d6c569b275de2d89",
+    "url": "/todo/static/css/main.4b26b95d.chunk.css"
+  },
+  {
+    "revision": "f5d4f18ba27fb711da5e",
+    "url": "/todo/static/js/2.9de16fbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.9de16fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e8d6c569b275de2d89",
+    "url": "/todo/static/js/main.c555324d.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
